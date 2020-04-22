70f36fbc757d9cfb88c481ff47156e6f68fd998f
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2020-04-22 16:56:52 -0300

    ajustando para CSV

--- consultas.js
+++ consultas.js
@@ -35,11 +35,9 @@ function processData(csvData){
   })
   .fromString(csvData)
   .then((csvRow)=>{
-    console.log("Processando CSV"); // => [["1","2","3"], ["4","5","6"], ["7","8","9"]]
+    console.log("Processando CSV...");
 
     $.each( csvRow, function( index, value ) {
-      console.log(value);
-      /*
       var linha = value;
       var key = linha[0];
       var val = linha[1];
@@ -56,7 +54,6 @@ function processData(csvData){
       // SVG
       $('#' + key +' >tspan').css('fill',corDaImagem);
       $('#' + key).css('fill',corDaImagem); // chakras
-      */
     });
 
   })
